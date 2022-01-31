e2f441a10b607f0d938aee97019360fc623aa427
dhinogz <davidhinojosauni@gmail.com>
2022-01-31 16:50:18 -0600

    Initial project commit with django-cookiecutter files

---
+++ tectalks/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from tectalks.users.models import User
+from tectalks.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
